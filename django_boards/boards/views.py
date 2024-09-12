eb2e0672c5e78b1e83607d92521874b6bdf141d5
Maria Campbell <interglobalmedia@gmail.com>
2024-09-12 18:46:48 -0400

    Add new_topic view

--- django_boards/boards/views.py
+++ django_boards/boards/views.py
@@ -12,5 +12,9 @@ def board_topics(request, id):
   board = get_object_or_404(Board, id=id)
   return render(request, 'topics.html', {'board': board})
 
+def new_topic(request, id):
+  board = get_object_or_404(Board, id=id)
+  return render(request, 'new_topic.html', {'board': board})
+
 
 
