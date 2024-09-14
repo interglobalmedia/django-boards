93bac3e87406dcf4077da30aa466c827f891966c
Maria Campbell <interglobalmedia@gmail.com>
2024-09-14 06:47:28 -0400

    Replace single quotes with double quotes

--- django_boards/django_boards/urls.py
+++ django_boards/django_boards/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 from boards import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('boards/<str:id>/', views.board_topics, name='board_topics'),
-    path('boards/<str:id>/new/', views.new_topic, name='new_topic'),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("boards/<str:id>/", views.board_topics, name="board_topics"),
+    path("boards/<str:id>/new/", views.new_topic, name="new_topic"),
+    path("admin/", admin.site.urls),
 ]
