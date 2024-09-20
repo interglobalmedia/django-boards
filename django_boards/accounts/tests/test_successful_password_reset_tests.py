883a91cb33d063525a6caac9f3459e44410bbfd3
Maria Campbell <interglobalmedia@gmail.com>
2024-09-20 08:00:26 -0400

    Add password-reset tests, urls, views, & templates

---
+++ django_boards/accounts/tests/test_successful_password_reset_tests.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core import mail
+from django.urls import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+class SuccessfulPasswordResetTests(TestCase):
+    def setUp(self):
+        email = 'john@doe.com'
+        User.objects.create_user(username='john', email=email, password='123abcdef')
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': email})
+
+    def test_redirection(self):
+        '''
+        A valid form submission should redirect the user to `password_reset_done` view
+        '''
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_send_password_reset_email(self):
+        self.assertEqual(1, len(mail.outbox))
