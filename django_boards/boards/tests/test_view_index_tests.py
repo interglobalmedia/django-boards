c31499d56e5d48b16b9dc00c9cf5bf2a4f57d654
Maria Campbell <interglobalmedia@gmail.com>
2024-09-28 15:54:57 -0400

    Add new boards tests structure

---
+++ django_boards/boards/tests/test_view_index_tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+from ..models import Board
+from ..views import index
+
+
+class IndexTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('index')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func, index)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'id': self.board.id})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
