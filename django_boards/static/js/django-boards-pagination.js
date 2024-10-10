e19984d72cd636c18191ca9e40e9af71eb7320b4
Maria Campbell <interglobalmedia@gmail.com>
2024-10-10 08:02:43 -0400

    Make querySlector into function

--- django_boards/static/js/django-boards-pagination.js
+++ django_boards/static/js/django-boards-pagination.js
@@ -1,5 +1,12 @@
-document.querySelector('.page-link', function (e) {
-  e.preventDefault()
-}).scrollIntoView({
-  behavior: "smooth", block: "end", inline: "nearest"
-})
+function paginationScrollBehavior() {
+  document.querySelector('.page-link', function (e) {
+    e.preventDefault()
+  }).scrollIntoView({
+    behavior: "smooth", block: "end", inline: "nearest"
+  })
+}
+
+// paginationScrollBehavior()
+
+export default paginationScrollBehavior
+
