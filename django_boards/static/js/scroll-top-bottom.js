579908ad82f241f7cbbddf66e05f0852cfe37217
Maria Campbell <interglobalmedia@gmail.com>
2024-10-10 08:03:34 -0400

    Remove visibility functions

--- django_boards/static/js/scroll-top-bottom.js
+++ django_boards/static/js/scroll-top-bottom.js
@@ -1,42 +1,18 @@
 // For scroll to top functionality
-const scrollTopButton = document.querySelector('.scroll.top')
+export const scrollTopButton = document.querySelector('.scroll.top')
 
-function scrollTop() {
+export function scrollTop() {
 	window.scrollTo(0, 0)
 }
 
-scrollTopButton.addEventListener('click', scrollTop)
+scrollTopButton.addEventListener('pointerdown', scrollTop)
 
 // Scroll to bottom functionality
-const scrollDownButton = document.querySelector('.scroll')
+export const scrollDownButton = document.querySelector('.scroll')
 
-function scrollStep() {
+export function scrollStep() {
 	window.scroll(0, window.scrollY + 200)
 	console.log(window.scrollY + 200)
 }
 
 scrollDownButton.addEventListener('pointerdown', scrollStep)
-
-// scroll top button disappears after click when scrollY == 0, but it reappears when scrollY is greater than 0 on scroll down.
-document.addEventListener('scroll', function () {
-	const scrollButton = document.querySelector('.scroll.top')
-	if (window.scrollY > 0) {
-		scrollButton.style.opacity = 1
-		scrollButton.style.transition = 'opacity 1s ease'
-	} else {
-		scrollButton.style.opacity = 0
-		scrollButton.style.transition = 'opacity 1s ease'
-	}
-})
-
-// scroll bottom button disappears after click of the scroll top button when scrollY == 0, but it reappears when scrollY is greater than 0 on scroll down.
-document.addEventListener('scroll', function () {
-	const scrollButton = document.querySelector('.scroll.bottom')
-	if (window.scrollY > 0) {
-		scrollButton.style.opacity = 1
-		scrollButton.style.transition = 'opacity 1s ease'
-	} else {
-		scrollButton.style.opacity = 0
-		scrollButton.style.transition = 'opacity 1s ease'
-	}
-})
