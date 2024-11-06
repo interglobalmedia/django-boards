8d958366d7f307b0a86bacea124bb3f10d3cfe7e
Maria Campbell <interglobalmedia@gmail.com>
2024-11-06 06:29:03 -0500

    Add profile-detail and profiles urls

--- django_boards/accounts/urls.py
+++ django_boards/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import profile, UserUpdateView
+from .views import profile, profile_detail, ProfileListView, UserUpdateView
 
 urlpatterns = [
     path('account/', UserUpdateView.as_view(), name='my_account'),
     path("profile/", profile, name="users-profile"),
+    path('profile-detail/<int:pk>/', profile_detail, name='profile'),
+    path("profiles/", ProfileListView.as_view(), name="users-profile-list"),
 ]
