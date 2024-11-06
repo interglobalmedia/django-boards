ea1e3ebc073302e9bf7c7241dbe12404b5b02c63
Maria Campbell <interglobalmedia@gmail.com>
2024-11-06 06:53:21 -0500

    BLV PLV paginate_by PLV get_context_data get_queryset reply_topic topic_post_url & redirect PUV sanitize form message

--- django_boards/boards/views.py
+++ django_boards/boards/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic import View
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from .forms import NewTopicForm
 from .forms import PostForm
 from .models import Board, Topic, Post
+from accounts.models import Profile
+from django.contrib.auth.models import User
+import math
+from django.views.generic.edit import CreateView
+import nh3
 
 # Create your views here.
 @method_decorator(login_required, name='dispatch')
@@ -26,7 +31,6 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-    
 
 class BoardListView(ListView):
     model = Board
@@ -37,7 +41,7 @@ class TopicListView(ListView):
     model = Topic
     context_object_name = 'topics'
     template_name = 'topics.html'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         kwargs['board'] = self.board
@@ -69,21 +73,26 @@ def new_topic(request, pk):
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
+@method_decorator(login_required, name='dispatch')
 class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
-    paginate_by = 3
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
     def get_queryset(self):
         self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
-        queryset = self.topic.posts.order_by('created_at')
+        queryset = list(reversed(self.topic.posts.order_by('created_at')))
         return queryset
 
 @login_required
@@ -96,7 +105,18 @@ def reply_topic(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
-        return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+            print(post.save, 'save the replied data')
+            topic.last_updated = timezone.now()
+            topic.save()
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
@@ -108,12 +128,40 @@ class PostUpdateView(UpdateView):
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+    success_url = "/"
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
+        if form:
+            form.instance.message = nh3.clean(form.instance.message,
+                tags={
+                    "a",
+                    "abbr",
+                    "acronym",
+                    "b",
+                    "blockquote",
+                    "code",
+                    "em",
+                    "i",
+                    "li",
+                    "ol",
+                    "strong",
+                    "ul",
+                    "s",
+                    "sup",
+                    "sub",
+                },
+                attributes={
+                    "a": {"href"},
+                    "abbr": {"title"},
+                    "acronym": {"title"},
+                },
+                url_schemes={"https"},
+                link_rel=None,)
+            super().form_valid(form)
         post = form.save(commit=False)
         post.updated_by = self.request.user
         post.updated_at = timezone.now()
