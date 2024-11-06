3d9982a0bea20d2d95992a54ba5551da74cfcb8d
Maria Campbell <interglobalmedia@gmail.com>
2024-11-06 07:04:09 -0500

    Remove scrolldown button refactor scrolltop

--- django_boards/static/js/app.js
+++ django_boards/static/js/app.js
@@ -1,24 +1,17 @@
-import paginationScrollBehavior from './django-boards-pagination.js'
 import {
-  scrollTop,
-  scrollStep,
   scrollTopButton,
-  scrollDownButton,
-} from './scroll-top-bottom.js'
-import toggleVisibilityScrollTop from './visibility-top.js'
-import toggleVisibilityScrollBottom from './visibility-bottom.js'
+  scrollFunction,
+  backToTop,
+} from './scroll-top.js'
 import { copyButton } from './copy-button.js'
 
-// const copyLinkInit = copyLink
-const toggleVisibilityScrollTopInit = toggleVisibilityScrollTop()
-const toggleVisibilityScrollBottomInit = toggleVisibilityScrollBottom()
-const paginationScrollBehaviorInit = paginationScrollBehavior()
+// When the user scrolls down 20px from the top of the document, show the button. This must come first.
+window.onscroll = function () {
+  scrollFunction()
+}
+
+// When the user clicks on the button, scroll to the top of the document. This must come after window.onscroll, otherwise it will not work.
+scrollTopButton.addEventListener("pointerdown", backToTop)
+
+// inits
 const copyButtonInit = copyButton()
-
-// For scroll to top functionality
-
-scrollTopButton.addEventListener('pointerdown', scrollTop)
-
-// Scroll to bottom functionality
-
-scrollDownButton.addEventListener('pointerdown', scrollStep)
