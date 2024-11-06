cfd3b306142ac68accd1ff1f1f45629c0b310d09
Maria Campbell <interglobalmedia@gmail.com>
2024-11-06 06:32:19 -0500

    Add HtmlSanitizedCharField class

--- django_boards/boards/forms.py
+++ django_boards/boards/forms.py
@@ -2,15 +2,47 @@ from django import forms
 from .models import Topic
 from django import forms
 from .models import Post
+import nh3
 
+class HtmlSanitizedCharField(forms.CharField):
+    def to_python(self, value):
+        value = super().to_python(value)
+        print(value, 'the Charfield value')
+        if value not in self.empty_values:
+            value = nh3.clean(
+                value,
+                # Allow only tags and attributes from our rich text editor
+                tags={
+                    "a",
+                    "abbr",
+                    "acronym",
+                    "b",
+                    "blockquote",
+                    "code",
+                    "em",
+                    "i",
+                    "li",
+                    "ol",
+                    "strong",
+                    "ul",
+                    "s",
+                    "sup",
+                    "sub",
+                },
+                attributes={
+                    "a": {"href"},
+                    "abbr": {"title"},
+                    "acronym": {"title"},
+                },
+                url_schemes={"https"},
+                link_rel=None,)
+        return value
 
 class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
         widget=forms.Textarea(
             attrs={"rows": 5, "placeholder": "What is on your mind?"}
         ),
-        max_length=4000,
-        help_text="The max length of the text is 4000.",
     )
 
     class Meta:
@@ -18,6 +50,7 @@ class NewTopicForm(forms.ModelForm):
         fields = ["subject", "message"]
 
 class PostForm(forms.ModelForm):
+    message = HtmlSanitizedCharField(widget=forms.Textarea)
     class Meta:
         model = Post
         fields = ['message', ]
