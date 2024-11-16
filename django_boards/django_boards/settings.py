9ab3160773f1c30e259dc2f4c2b99283ec6316e2
Maria Campbell <interglobalmedia@gmail.com>
2024-11-16 08:38:05 -0500

    Add ISHEROKU_APP and reconfig static files for heroku

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -39,15 +39,34 @@ AVATAR_PROVIDERS = (
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
+# also explicitly exclude CI:
+# https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
-ALLOWED_HOSTS = [
-    "http://0.0.0.0:5006",
-    "localhost",
-    "127.0.0.1",
-    "https://django-boards-4ce48625014e.herokuapp.com/",
-]
+# SECURITY WARNING: don't run with debug turned on in production!
+if not IS_HEROKU_APP:
+    DEBUG = True
+
+# On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
+# validation of the Host header in the incoming HTTP request. On other platforms you may need to
+# list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
+# https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-ALLOWED_HOSTS
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+
+# ALLOWED_HOSTS = [
+#     "http://0.0.0.0:5006",
+#     "localhost",
+#     "127.0.0.1",
+#     "https://django-boards-4ce48625014e.herokuapp.com/",
+# ]
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
@@ -95,12 +114,15 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",  # new
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
