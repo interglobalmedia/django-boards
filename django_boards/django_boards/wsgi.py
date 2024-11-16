4ceb0553fbddbc7acc787e82136313fe66d76182
Maria Campbell <interglobalmedia@gmail.com>
2024-11-16 11:15:26 -0500

    Add pythonpath revert to original wsgi.py

--- django_boards/django_boards/wsgi.py
+++ django_boards/django_boards/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_boards.settings')
 
-# application = get_wsgi_application()
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
