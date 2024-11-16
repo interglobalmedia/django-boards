6e3568e16a5729d66848376a25a47e5ccbeba172
Maria Campbell <interglobalmedia@gmail.com>
2024-11-16 07:21:05 -0500

    Add whitenoise middleware / to static

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # new
 ]
 
 STORAGES = {
@@ -167,7 +168,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' # added beginning forward slash
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
