9e10fd62af18c37784ab9717b0abdd8e2ba3830e
Maria Campbell <interglobalmedia@gmail.com>
2024-11-25 21:00:54 -0500

    Keep only wildcard

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -47,7 +47,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-boards-71e292501730.herokuapp.com', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
