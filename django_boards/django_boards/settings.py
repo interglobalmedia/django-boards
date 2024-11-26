fca2dad2fa855a08a0c69f906bfe3e1edb6c7a0e
Maria Campbell <interglobalmedia@gmail.com>
2024-11-25 22:13:05 -0500

    Change order of allowed hosts

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-boards-71e292501730.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://django-boards-71e292501730.herokuapp.com']
 
 # Application definition
 
