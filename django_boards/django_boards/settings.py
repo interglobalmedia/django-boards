a3e05fc7aec6a4d02b24bd15041383a8c1d7873b
Maria Campbell <interglobalmedia@gmail.com>
2024-11-25 20:33:29 -0500

    Add back DB url hack

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -128,8 +128,10 @@ WSGI_APPLICATION = 'django_boards.wsgi.application'
 #     }
 # }
 
+DATABASE_URL = str(os.getenv('DATABASE_URL'))
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': DATABASE_URL
 }
 
 # Password validation
