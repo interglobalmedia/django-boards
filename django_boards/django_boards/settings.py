220be2d92cd760e50d9d2137ecf91e2a16bb4302
Maria Campbell <interglobalmedia@gmail.com>
2024-11-25 22:06:50 -0500

    remove max_age

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'django_boards.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config()
 }
 
 # Password validation
