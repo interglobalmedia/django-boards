6731423df54bc365c2c9ae0b6359500e7f2a8455
Maria Campbell <interglobalmedia@gmail.com>
2024-11-27 07:26:58 -0500

    remove dot from localhost and double colons

--- django_boards/django_boards/settings.py
+++ django_boards/django_boards/settings.py
@@ -54,7 +54,7 @@ if not IS_HEROKU_APP:
 if IS_HEROKU_APP:
     ALLOWED_HOSTS = ["*"]
 else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0", "[::]"]
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
