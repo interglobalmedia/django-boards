b29b0e8945c946abc1021fe27499ed619c636c19
Maria Campbell <interglobalmedia@gmail.com>
2024-12-01 18:28:13 -0500

    Remove storage attribute

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-from utils.storage_backends import PublicMediaStorage
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images', storage=PublicMediaStorage())
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
