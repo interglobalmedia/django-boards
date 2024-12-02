4074816a525fed4387ee8bd53a808fd00273c0ee
Maria Campbell <interglobalmedia@gmail.com>
2024-12-01 19:20:09 -0500

    Add back PublicMediaStorage and add _profile_avatar_upload_path

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from utils.storage_backends import PublicMediaStorage
+import pathlib
+
+def _profile_avatar_upload_path(instance, filename):
+    """Provides a clean upload path for user avatar images
+    """
+    file_extension = pathlib.Path(filename).suffix
+    return f'profile_images/{instance.id}{file_extension}'
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images', storage=PublicMediaStorage())
+    avatar = models.ImageField(default='default.jpg', upload_to='_profile_avatar_upload_path', storage=PublicMediaStorage())
     bio = models.TextField()
 
     def __str__(self):
