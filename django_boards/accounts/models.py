5b282ba819fc4ce272576be7b019bbf0b18142fc
Maria Campbell <interglobalmedia@gmail.com>
2024-12-02 08:56:10 -0500

    Remove path func replace with profile_images

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -7,17 +7,11 @@ from django.core.files.storage import default_storage
 from io import BytesIO
 import os
 
-def _profile_avatar_upload_path(instance, filename):
-    """Provides a clean upload path for user avatar images
-    """
-    file_extension = pathlib.Path(filename).suffix
-    return f'profile_images/{instance.id}{file_extension}'
-
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='_profile_avatar_upload_path', storage=PublicMediaStorage())
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images', storage=PublicMediaStorage())
     bio = models.TextField()
 
     def __str__(self):
