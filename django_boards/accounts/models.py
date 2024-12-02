53baec2e0e7e750da2c92c8bdfcc5c84eb421ce5
Maria Campbell <interglobalmedia@gmail.com>
2024-12-02 06:40:42 -0500

    remove default.jpg

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -10,13 +10,13 @@ def _profile_avatar_upload_path(instance, filename):
     """Provides a clean upload path for user avatar images
     """
     file_extension = pathlib.Path(filename).suffix
-    return f'media/public/profile_images/{instance.id}{file_extension}'
+    return f'profile_images/{instance.id}{file_extension}'
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='_profile_avatar_upload_path', storage=PublicMediaStorage())
+    avatar = models.ImageField(upload_to='_profile_avatar_upload_path', storage=PublicMediaStorage())
     bio = models.TextField()
 
     def __str__(self):
