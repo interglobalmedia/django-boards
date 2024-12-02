b92a2551e7706969c95cfbc6633adcf78d50e74f
Maria Campbell <interglobalmedia@gmail.com>
2024-12-02 07:13:53 -0500

    Add relative path

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -5,12 +5,16 @@ from utils.storage_backends import PublicMediaStorage
 import pathlib
 from django.core.files.storage import default_storage
 from io import BytesIO
+import os
 
 def _profile_avatar_upload_path(instance, filename):
     """Provides a clean upload path for user avatar images
     """
     file_extension = pathlib.Path(filename).suffix
-    return f'profile_images/{instance.id}{file_extension}'
+    settings_dir = os.path.dirname(__file__)
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))
+    PROFILE_AVATARS_DIR = os.path.join(PROJECT_ROOT, 'django_boards/media/profile_images/')
+    return f'{PROFILE_AVATARS_DIR}/{instance.id}{file_extension}'
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
