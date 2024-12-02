762398d4f05c04310941feb6a4d7a616f5f7cd06
Maria Campbell <interglobalmedia@gmail.com>
2024-12-02 08:15:23 -0500

    Add path to self.avatar in save method

--- django_boards/accounts/models.py
+++ django_boards/accounts/models.py
@@ -35,7 +35,7 @@ class Profile(models.Model):
         if img.height > 80 or img.width > 80:
             new_img = (80, 80)
             img.thumbnail(new_img)
-            img.save(self.avatar.name)
+            img.save(self.avatar.path)
 
         def __str__(self):
             return self.user.username
