6ae4ada6103b260f7a22a9f617099a448a57e0f0
Maria Campbell <interglobalmedia@gmail.com>
2024-12-20 09:48:03 -0500

    Add render conditional for production db

--- django_boards/settings.py
+++ django_boards/settings.py
@@ -112,17 +112,16 @@ WSGI_APPLICATION = 'django_boards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+if 'RENDER' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    }
 
 
 # Password validation
