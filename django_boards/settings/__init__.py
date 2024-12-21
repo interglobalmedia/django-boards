af3086404689da96f1cf2f40ed1349946a6be218
Maria Campbell <interglobalmedia@gmail.com>
2024-12-21 16:31:33 -0500

    Remove settings dir

--- django_boards/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-# you need to set "myproject = 'prod'" as an environment variable
-# in your OS (on which your website is hosted)
-PROD = os.environ.get('PROD')
-if PROD == True:
-   from .prod import *
-else:
-   from .dev import *
