213c296f53cd1ac11a3671c8fb685446a2c27b18
Maria Campbell <interglobalmedia@gmail.com>
2024-12-27 16:56:55 -0500

    Remove usable pasword

--- accounts/tests/test_form_signup_test.py
+++ accounts/tests/test_form_signup_test.py
@@ -4,6 +4,6 @@ from ..forms import SignUpForm
 class SignUpFormTest(TestCase):
     def test_form_has_fields(self):
         form = SignUpForm()
-        expected = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'usable_password']
+        expected = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
