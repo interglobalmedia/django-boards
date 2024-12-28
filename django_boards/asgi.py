d143a63d5479aff76a30c2fc127e054790795218
Maria Campbell <interglobalmedia@gmail.com>
2024-12-28 17:05:04 -0500

    Move DJANGO_SETTINGS_MODULE to top

--- django_boards/asgi.py
+++ django_boards/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_boards.settings.production')
+
+from django.core.asgi import get_asgi_application
 
 application = get_asgi_application()
