4c2af39bda71568384c24c08cd8c8628e1e78716
Maria Campbell <interglobalmedia@gmail.com>
2024-12-28 12:10:51 -0500

    Define Debug in both environments

--- django_boards/settings/development.py
+++ django_boards/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # using decouple
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 # using decouple to point to ALLOWED_HOSTS env var locally
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
