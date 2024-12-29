7b55bada05e5c9b503250f7dd917dba0e90a154b
Maria Campbell <interglobalmedia@gmail.com>
2024-12-28 23:16:02 -0500

    Add back os.environ for MEDIA_ROOT

--- django_boards/settings/production.py
+++ django_boards/settings/production.py
@@ -7,8 +7,8 @@ DEBUG = os.environ.get('DEBUG')
 # using decouple to point to ALLOWED_HOSTS env var on render
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
-# MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "/opt/render/project/src/mediafiles")
-MEDIA_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'mediafiles'))
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "/opt/render/project/src/mediafiles")
+# MEDIA_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'mediafiles'))
 print(MEDIA_ROOT, "media root")
 
 
