cae2c4773190fbfcb367e799648dbc455da82a15
Maria Campbell <interglobalmedia@gmail.com>
2024-12-28 20:30:08 -0500

    Uncomment DATABASES

--- django_boards/settings/production.py
+++ django_boards/settings/production.py
@@ -14,9 +14,9 @@ print(MEDIA_ROOT, "media root")
 
 # Render database
 # using decouple
-# DATABASES = {
-#     "default": dj_database_url.config(default=config("DATABASE_URL"), conn_max_age=600),
-# }
+DATABASES = {
+    "default": dj_database_url.config(default=config("DATABASE_URL"), conn_max_age=600),
+}
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 # Tell Django to copy static assets into a path called `static` (this is specific to Render)
