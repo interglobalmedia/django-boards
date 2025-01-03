466c54b4b7c70c716ea846826f3a161144853c92
Maria Campbell <interglobalmedia@gmail.com>
2025-01-03 07:42:55 -0500

    Update comment

--- manage.py
+++ manage.py
@@ -2,21 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 from dotenv import load_dotenv
-
 load_dotenv()  # take environment variables from .env
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 from django_boards.settings import development, production
 import sys
 
-
 def main():
     """Run administrative tasks."""
+    # in order for development env to work, must be set to development here locally. Must be set to production before deploying to Render. The "if elif" condition takes care of that.
     DEVELOPMENT_ENVIRONMENT = os.getenv("ENVIRONMENT")
     PRODUCTION_ENVIRONMENT = os.environ.get("ENVIRONMENT")
     if development:
         print(development, 'in development')
-        # in order for development env to work, must be set to development here locally. This condition (hopefully) takes care of that. We shall see if "if/elif" works in production. Works in development.
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DEVELOPMENT_ENVIRONMENT")
     elif production:
         print(production, 'in production')
