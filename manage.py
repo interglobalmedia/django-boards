73038bccf2a295c959ce925b1b07ea4ecb7f3062
Maria Campbell <interglobalmedia@gmail.com>
2025-01-03 07:03:55 -0500

    Add dev vs prod condition and load dotenv

--- manage.py
+++ manage.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+from django_boards.settings import development, production
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    # in order for development env to work, must be set to development here locally. But change before committing changes and deploying to production.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_boards.settings.production')
+    DEVELOPMENT_ENVIRONMENT = os.getenv("ENVIRONMENT")
+    PRODUCTION_ENVIRONMENT = os.environ.get("ENVIRONMENT")
+    if development:
+        print(development, 'in development')
+        # in order for development env to work, must be set to development here locally. This condition (hopefully) takes care of that. We shall see if "if/elif" works in production. Works in development.
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DEVELOPMENT_ENVIRONMENT")
+    elif production:
+        print(production, 'in production')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PRODUCTION_ENVIRONMENT")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,5 +32,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
