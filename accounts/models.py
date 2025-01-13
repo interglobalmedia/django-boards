4d1d1470fdc038749a1d93e426860fc9023324d6
Maria Campbell <interglobalmedia@gmail.com>
2025-01-13 15:08:28 -0500

    Add lint formatting

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from PIL import Image
+
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(default="default.jpg", upload_to="profile_images")
     bio = models.TextField()
 
     def __str__(self):
