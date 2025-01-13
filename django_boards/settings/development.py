49143c7494aa495c33e627becbeef1fb074fc25f
Maria Campbell <interglobalmedia@gmail.com>
2025-01-13 15:29:25 -0500

    Add lint formatting

--- django_boards/settings/development.py
+++ django_boards/settings/development.py
@@ -10,12 +10,12 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 # For user uploaded files locally
 MEDIA_ROOT = os.getenv("MEDIA_ROOT")
 
-print(MEDIA_ROOT, 'media root in development')
+print(MEDIA_ROOT, "media root in development")
 
 WSGI_APPLICATION = "django_boards.wsgi.application"
 
 # correct absolute path in which db.sqlite3 resides
-CURRENT_DIR= '/Users/mariacam/Python-Development/django-boards/'
+CURRENT_DIR = "/Users/mariacam/Python-Development/django-boards/"
 
 # Local Database. Comment out before deploying to production
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
